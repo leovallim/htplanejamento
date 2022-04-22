0dd2d7ea45879f7eb526d49200807c87be0c3aa2
Leo Vallim <leo.vallim@gmail.com>
2022-04-22 14:27:18 -0300

    Removendo paginas de opções

--- core/option-pages.php
+++ core/option-pages.php
@@ -13,41 +13,5 @@ function ht_register_option_pages(){
             "icon_url"          => "dashicons-screenoptions",
             "position"          => 2,
         ]);
-        $info = acf_add_options_page([
-            "page_title"        => "Institucional",
-            "menu_title"        => "Institucional",
-            "menu_slug"         => "ht_branding_conf",
-            "parent_slug"       => $general["menu_slug"],
-        ]);
-        $contact = acf_add_options_page([
-            "page_title"        => "Contato e Social",
-            "menu_title"        => "Contato e Social",
-            "menu_slug"         => "ht_contact_conf",
-            "parent_slug"       => $general["menu_slug"],
-        ]);
-        $header = acf_add_options_page([
-            "page_title"        => "Cabeçalho",
-            "menu_title"        => "Cabeçalho",
-            "menu_slug"         => "ht_header_conf",
-            "parent_slug"       => $general["menu_slug"],
-        ]);
-        $footer = acf_add_options_page([
-            "page_title"        => "Rodapé",
-            "menu_title"        => "Rodapé",
-            "menu_slug"         => "ht_footer_conf",
-            "parent_slug"       => $general["menu_slug"],
-        ]);
-        $nav = acf_add_options_page([
-            "page_title"        => "Depoimentos",
-            "menu_title"        => "Depoimentos",
-            "menu_slug"         => "ht_testimonial_conf",
-            "parent_slug"       => $general["menu_slug"],
-        ]);
-        $scripts = acf_add_options_page([
-            "page_title"        => "Scripts",
-            "menu_title"        => "Scripts",
-            "menu_slug"         => "ht_script_conf",
-            "parent_slug"       => $general["menu_slug"],
-        ]);
     }
 }
